4cc65731913dc60a0cdff5d919746f954ec5fb75
Wohnlich, Eric (IMS) <wohnliche@imsweb.com>
2015-06-24 15:13:14 -0400

    don't trash blob files

--- ims/trashcan/can.py
+++ ims/trashcan/can.py
@@ -21,18 +21,21 @@ class PloneTrashCan(Folder):
 
     manage_options = [{'label':'Trash Can','action':'manage_main'},
                       {'label':'Configuration','action':'manage_propertiesForm'}]
-    
+
     _properties=({'id':'disposal_frequency','type':'int','mode':'w'},)
-      
+
     def trash(self, ob):
       """ trash the item """
-      id = generate_id(ob.getId())
-      title = ob.Title()
-      data = self.zexpickle(ob)
-      opath = '/'.join(ob.getPhysicalPath()[:-1])
-      _trash = TrashedItem(id,title,data,opath)
-      self._setObject(id,_trash,suppress_events=True)
-    
+      from plone.app.blob.interfaces import IATBlob
+      if not IATBlob.providedBy(ob):
+        # trashing BLOBs can take too much memory
+        id = generate_id(ob.getId())
+        title = ob.Title()
+        data = self.zexpickle(ob)
+        opath = '/'.join(ob.getPhysicalPath()[:-1])
+        _trash = TrashedItem(id,title,data,opath)
+        self._setObject(id,_trash,suppress_events=True)
+
     def restore(self, id):
       """ restore the item to its original path
           if it exists, otherwise root """
@@ -58,26 +61,26 @@ class PloneTrashCan(Folder):
       """ unpickle + remove zexp wrapper """
       f = StringIO()
       f.write(data)
-    
+
       # we HAVE to skip the first four characters, which are just
       # 'ZEXP', because _importDuringCommit does length validation
       # with the assumption that the file stream is already at this
       # point, not at SOF
       f.seek(4)
-    
+
       conn = self._p_jar
       import transaction as t
-      
+
       return_oid_list = []
       conn._import = f, return_oid_list
       conn._register()
-      
+
       t.savepoint(optimistic=True)
       if return_oid_list:
           return conn.get(return_oid_list[0])
       else:
           return None
-        
+
     security.declareProtected(ManageTrash, 'deleteExpired')
     def deleteExpired(self):
       """Delete all of the content that is expired
@@ -97,5 +100,5 @@ class PloneTrashCan(Folder):
       msg=u'%s has been restored.' % id
       if REQUEST is not None:
         return self.manage_main(self, REQUEST, manage_tabs_message=msg)
-    
+
     __call__ = deleteExpired
