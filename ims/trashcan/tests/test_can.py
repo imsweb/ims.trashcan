185c45b8f01da0d5f8780329a219545a599dcb79
wohnlice <wohnlice@imsweb.com>
2019-03-29 12:56:03 -0400

    test that large files are not trashed

--- ims/trashcan/tests/test_can.py
+++ ims/trashcan/tests/test_can.py
@@ -1,5 +1,6 @@
 import plone.api
 
+from plone.namedfile.file import NamedBlobFile
 from . import base
 
 
@@ -23,6 +24,13 @@ class TestTrashCan(base.IntegrationTestCase):
         plone.api.content.delete(cfile)
         self.assertEqual(len(self.can.objectIds()), 0)
 
+    def test_file_limit(self):
+        _file = plone.api.content.create(id='file1', type='File', title='My file', container=self.portal)
+        _file.file = NamedBlobFile()
+        _file.file.data = u'hello, world'
+        _file.file.__dict__['size'] = 1e10  # fake it
+        plone.api.content.delete(_file)
+        self.assertEqual(len(self.can.objectIds()), 0)
 
 
 def test_suite():
