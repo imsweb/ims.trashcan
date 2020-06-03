13c221273ea31e6e15f8cf1bc5647a668b9c3dc4
wohnlice <wohnlice@imsweb.com>
2020-06-03 17:46:49 -0400

    more test cleanup

--- ims/trashcan/testing.py
+++ ims/trashcan/testing.py
@@ -1,17 +1,15 @@
 from plone.app.testing import PloneSandboxLayer, IntegrationTesting, FunctionalTesting, applyProfile
-from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
+from plone.app.testing import PLONE_FIXTURE
 import ims.trashcan
-import collective.js.jqueryui
 
 
 class TrashcanSiteLayer(PloneSandboxLayer):
-    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
+    defaultBases = (PLONE_FIXTURE,)
 
     def setUpZope(self, app, configuration_context):
         # Load any other ZCML that is required for your tests.
         # The z3c.autoinclude feature is disabled in the Plone fixture base
         # layer.
-        self.loadZCML(package=collective.js.jqueryui)
         self.loadZCML(package=ims.trashcan)
 
     def setUpPloneSite(self, portal):
